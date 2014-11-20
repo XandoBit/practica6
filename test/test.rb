8be78289fa432aa06c7533c32d96f777ce2fe4ed
Rushil Lakhani <alu0100537031@ull.edu.es>
2014-11-20 19:18:28 +0000

    gemas selenium y coveralls

--- test/test.rb
+++ test/test.rb
@@ -1,44 +1,17 @@
+require 'capybara' # loading capybara
+require 'capybara/dsl'
+require 'rack/test'
+require 'coveralls'
+
+Coveralls.wear!
+
+Capybara.default_driver = :selenium 
+
 ENV['RACK_ENV'] = 'test'
 
-require 'minitest/autorun'
-require 'rack/test'
-require_relative '../chat.rb'
-
-
-include Rack::Test::Methods
-
-def chat
-    Sinatra::Application
-end
-
-describe "Chat SYTW" do
-    
-    
-    it "Should return index" do
-        get '/'
-        assert last_response.ok?
-    end
-    
-    it "should return title" do
-        get '/'
-        assert_match "<title>Chat SYTW</title>", last_response.body
-    end
-    
-    it "should return form" do
-        get '/'
-        assert_match "<p ><b>Escribe tu username aquí...</b></p>", last_response.body
-    end
-
-
-    it "Debe cargar la foto de fondo" do
-	get '/'
-	assert_match '<img src="/fondo.jpg" />', last_response.body
-    end
-
-    it "should return foot" do
-        get '/'
-        assert_match " %p.pull-right SYTW 2014. Autores: Rushil Lakhani Lakhani & Adán Rafael López Lecuona.  ", last_response.body
-    end
-    
-    
+RSpec.configure do |config|
+  config.include Capybara::DSL
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+  config.order = :random
 end
